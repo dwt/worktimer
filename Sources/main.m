ffd3915e8c40e48cdbcda35c694e9db9fb0e42de
Martin Häcker <mhaecker@mac.com>
2019-10-28 18:30:37 +0100

    Hide UI again when running unit tests

--- Sources/main.m
+++ Sources/main.m
@@ -8,8 +8,20 @@
 
 #import <Cocoa/Cocoa.h>
 
+BOOL isRunningUnitTests() {
+    return nil != NSClassFromString(@"XCTestCase");
+}
+
+int runApplicationWithoutLoadingGUI() {
+    [NSApplication sharedApplication];
+    [NSApp run];
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
+    if (isRunningUnitTests())
+        return runApplicationWithoutLoadingGUI();
+    
     return NSApplicationMain(argc,  (const char **) argv);
 }
